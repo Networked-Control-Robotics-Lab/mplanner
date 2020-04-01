ddad3631a731362b6ef7f4a22155048caa9de494
Shengwen Cheng <c.shengwen.tw@gmail.com>
2020-04-02 02:15:34 +0800

    port serial functions and mavlink_v2

---
+++ serial.cpp
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <inttypes.h>
+#include <string>
+#include "ros/ros.h"
+
+using namespace std;
+
+int serial_fd = 0;
+
+void serial_init(char *port_name, int baudrate)
+{
+	//open the port
+	serial_fd = open(port_name, O_RDWR | O_NOCTTY | O_NONBLOCK);
+
+	if(serial_fd == -1) {
+		ROS_FATAL("Failed to open the serial port.");
+		exit(0);
+	}
+
+	//config the port
+	struct termios options;
+
+	tcgetattr(serial_fd, &options);
+
+	options.c_cflag = CS8 | CLOCAL | CREAD;
+	options.c_iflag = IGNPAR;
+	options.c_oflag = 0;
+	options.c_lflag = 0;
+
+	switch(baudrate) {
+	case 9600:
+		options.c_cflag |= B9600;
+		break;
+	case 57600:
+		options.c_cflag |= B57600;
+		break;
+	case 115200:
+		options.c_cflag |= B115200;
+		break;
+	default:
+		ROS_FATAL("Unknown baudrate. try 9600, 57600, 115200 or check \"serial.cpp\".");
+		exit(0);
+	}
+
+	tcflush(serial_fd, TCIFLUSH);
+	tcsetattr(serial_fd, TCSANOW, &options);
+}
+
+void serial_puts(char *s, size_t size)
+{
+	write(serial_fd, s, size);
+}
